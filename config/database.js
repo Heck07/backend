1a2fefc56d77e104c5a354a6cc3c7d14ec0fa272
Heck07 <durand.romain7@yahoo.com>
2024-10-22 08:59:07 +0200

    database auto restart

--- config/database.js
+++ config/database.js
@@ -1,18 +1,33 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.error('Erreur de connexion à la base de données :', err);
-    throw err;
-  }
-  console.log('Connecté à la base de données MySQL');
-});
+function connectDatabase() {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+
+  db.connect((err) => {
+    if (err) {
+      console.error('Erreur de connexion à la base de données :', err);
+      setTimeout(connectDatabase, 2000); // Attendez 2 secondes avant de réessayer
+    } else {
+      console.log('Connecté à la base de données MySQL');
+    }
+  });
+
+  db.on('error', (err) => {
+    console.error('Erreur avec la connexion à la base de données :', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.fatal) {
+      connectDatabase(); // Reconnectez automatiquement si la connexion est perdue
+    }
+  });
+}
+
+connectDatabase();
 
 module.exports = db;
