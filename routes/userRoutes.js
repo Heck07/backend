9e911f9c4e6369ae3deb0a1e91b46dc5c57e6119
Heck07 <durand.romain7@yahoo.com>
2024-10-24 09:20:37 +0200

    Categories Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const categoryController = require('../controllers/categoryController');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 const authenticateToken = require('../middlewares/authMiddleware');
-const authMiddleware = require('../middlewares/authMiddleware');
 
+// Récupérer toutes les catégories (accessible uniquement aux utilisateurs authentifiés)
+router.get('/', authenticateToken, categoryController.getAllCategories);
 
-router.get('/', authMiddleware, roleMiddleware(['admin']), userController.getAllUsers);
-router.put('/:id', authMiddleware, roleMiddleware(['admin']), userController.updateUser);
-router.delete('/:id', authMiddleware, roleMiddleware(['admin']), userController.deleteUser);
+// Ajouter une catégorie (accessible uniquement aux administrateurs)
+router.post('/', authenticateToken, roleMiddleware('admin'), categoryController.addCategory);
 
+// Mettre à jour une catégorie (accessible uniquement aux administrateurs)
+router.put('/:id', authenticateToken, roleMiddleware('admin'), categoryController.updateCategory);
 
-router.get('/me', authenticateToken, userController.getUserDetails);
-
-router.put('/me/password', authenticateToken, userController.updateUserPassword);
-
+// Supprimer une catégorie (accessible uniquement aux administrateurs)
+router.delete('/:id', authenticateToken, roleMiddleware('admin'), categoryController.deleteCategory);
 
 module.exports = router;
